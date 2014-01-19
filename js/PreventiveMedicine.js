7f3a111271a9e370f00ff2ef041abccdc34be35b
Rich Brown <richb.hanover@gmail.com>
2014-01-19 06:53:02 -0500

    About to start parsing rules

--- js/PreventiveMedicine.js
+++ js/PreventiveMedicine.js
@@ -4,29 +4,52 @@
 // see http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
 
 var PreventiveMedicine = (function ($) {
+    // private to this module
     var my = {},
         age = 0,
         height = 0,
-        weight = 0;
+        weight = 0,
         bmi = 0,
         packs = 0,
         yearsSmoked=0,
-        packyears = 0,
+        quitYear = 0,
         drinks = 0;
-
 
 //    function privateMethod() {
 //        // ...
 //    };
+    function ProcessRules() {
+        alert(globalrulesobject.rules.length);
+
+    }
 
     my.init= function(){
-        alert("Called the PreventiveMedicine.init method");
+
     };
 
     my.moduleProperty = 1;
     my.GetRecommendations = function () {
 
-        return "<em>Hi Rich!</em>";
+        age    = $("#numAge").val();
+        height = $("#numHeightIn").val();
+        weight = $("#numWeightLb").val();
+        if (weight != "0" && weight != ""){
+
+            h = parseFloat(height);
+            w = parseFloat(weight);
+            bmi    = (703 * w)/(h*h);
+            $("#numBmi").val(bmi.toPrecision(3));
+        } else {
+            $("#numBmi").val("-");
+        }
+
+        packs = $("#numPacks").val();
+        yearsSmoked = $("#numSmokeYears").val();
+        quitYear = $("#numQuitInYear").val();
+        drinks = $("#numDrinksPerWk").val();
+
+        var recs = ProcessRules();
+        return recs;
     };
 
     return my;      // return the PreventiveMedicine module
